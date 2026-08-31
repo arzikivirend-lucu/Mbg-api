44a5dd1e0655a52e8e08e40ec11475be5d493b03
arzikivirend-lucu <arzikivirend@gmail.com>
2026-08-31 21:13:03 +0700

    Delete mbg-api-platform-updated/mbg-api-platform directory

--- mbg-api-platform-updated/mbg-api-platform/server.js
+++
@@ -1,6 +0,0 @@
-const app = require("./src/app");
-
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Mindbot Genius API Platform jalan di http://localhost:${PORT}`);
-});
